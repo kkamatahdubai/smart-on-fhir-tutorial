a077af1c537f60186de42af959bce6ecc6021bf3
kkamatahdubai <kkamat@ahdubai.com>
2026-03-06 01:00:00 +0400

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -53,7 +53,7 @@
           p.fname = fname;
           p.lname = lname;
           p.height = getQuantityValueAndUnit(height[0]);
-          p.temperature = temp;
+          p.temperature = getQuantityValueAndUnit(temp[0]);
 
           if (typeof systolicbp != 'undefined')  {
             p.systolicbp = systolicbp;
